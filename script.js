ff13076a9a2948f8589f733e3fb43fcb0948015e
Calo <79152594+Objectivitix@users.noreply.github.com>
2022-10-17 19:40:48 -0400

    Stylise close button on modal; add SVG system

--- script.js
+++ script.js
@@ -104,3 +104,20 @@ function Book(title, author, pageCount, readStatus) {
 Book.prototype.toggleRead = function () {
   this.readStatus = !this.readStatus;
 }
+
+/* SVGs */
+
+const icons = document.querySelectorAll("div[data-icon]");
+
+icons.forEach(icon => {
+  const svgNS = "http://www.w3.org/2000/svg";
+  const svg = document.createElementNS(svgNS, "svg");
+  svg.setAttribute("xmlns", svgNS);
+  svg.setAttribute("viewBox", "0 0 24 24");
+
+  const use = document.createElementNS(svgNS, "use");
+  use.setAttribute("href", `#${icon.dataset.icon}-icon`);
+  svg.appendChild(use);
+
+  icon.replaceWith(svg);
+});
